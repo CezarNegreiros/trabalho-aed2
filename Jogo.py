0ad738120c2aff58c7341fc14344a5e093f06de5
Gabriel-Takeda <gabrielseiditsilva@gmail.com>
2023-11-03 17:07:15 -0400

    ajuste das classes

--- Jogo.py
+++ Jogo.py
@@ -1,10 +1,76 @@
-import Player
-import Labirinto
+from Jogador import Jogador
+from Labirinto import Labirinto
+import msvcrt
+
+def labirinto_in_game(labirinto, jogador):
+    while True:
+
+        imprimir_labirinto(labirinto)
+        key = msvcrt.getch()
+
+        if key == b'\xe0':
+            key = msvcrt.getch()
+            if key == b'H':
+                andar_para_cima(labirinto, jogador)
+            elif key == b'P':
+                andar_para_baixo(labirinto, jogador)
+            elif key == b'K':
+                andar_para_esquerda(labirinto,jogador)
+            elif key == b'M':
+                andar_para_direita(labirinto, jogador)
+        elif key == b'w':
+            andar_para_cima(labirinto, jogador)
+        elif key == b'a':
+            andar_para_esquerda(labirinto, jogador)
+        elif key == b's':
+            andar_para_baixo(labirinto, jogador)
+        elif key == b'd':
+            andar_para_direita(labirinto, jogador)
+        elif key == b'q':
+            break
+
+        if verifica_vitoria(labirinto, jogador):
+            print('VITORIAA (/^▽^)/')
+            break
+        else:
+            print('+-'*15)
+
+def mover_jogador(labirinto,  jogador,new_x, new_y):
+        if labirinto.maze[new_x][new_y] != 'X' and new_x > -1 and new_x < labirinto.x + 1 and new_y > -1 and new_y < labirinto.y +1:
+            labirinto.maze[new_x][new_y] = '*'
+            labirinto.maze[jogador.posicao_x][jogador.posicao_y] = '0'
+            if jogador.posicao_y != new_y:
+                jogador.posicao_y = new_y
+            if jogador.posicao_x != new_x:
+                jogador.posicao_x = new_x
+        else:
+            print('MOVIMENTO INVALIDO')
+
+def andar_para_cima(labirinto, jogador):
+    mover_jogador(labirinto, jogador ,jogador.posicao_x - 1, jogador.posicao_y)
+
+def andar_para_baixo(labirinto, jogador):
+    mover_jogador(labirinto, jogador ,jogador.posicao_x + 1, jogador.posicao_y)
+
+def andar_para_esquerda(labirinto,jogador):
+    mover_jogador(labirinto, jogador ,jogador.posicao_x, jogador.posicao_y - 1)
+
+def andar_para_direita(labirinto,jogador):
+    mover_jogador(labirinto, jogador ,jogador.posicao_x, jogador.posicao_y + 1)
+
+    
+def verifica_vitoria(labirinto, jogaor):
+    return labirinto.x - 2 == jogador.posicao_x and labirinto.y - 1 == jogador.posicao_y
+
+def imprimir_labirinto(labirinto):
+    for i in range(labirinto.x):
+        for j in range(labirinto.y):
+            print(labirinto.maze[i][j], end='')
+        print()
 
 #Cria jogador
-jogador = Player(1, 0)
-
+jogador = Jogador(1, 0)
 #Cria Labirinto
-labirinto = Labirinto()
-matriz = labirinto.criar_labirinto()
-labirinto.imprimir_labirinto(matriz)
+labirinto =  Labirinto()
+#jogo
+labirinto_in_game(labirinto,jogador)
--- Labirinto.py
+++ Labirinto.py
@@ -1,59 +1,15 @@
 import random
-import msvcrt
-
-"""
-Módulo de Geração de Labirinto
-
-Este módulo define a classe Labirinto, que permite criar e imprimir um labirinto aleatório
-usando o algoritmo de Prim's.
-
-Classe:
-    Labirinto: Representa um labirinto com métodos para criar e imprimir o labirinto.
-
-Uso:
-    maze = Labirinto()       # Cria uma instância do Labirinto
-    maze.imprimir_labirinto()  # Imprime o labirinto gerado
-
-Autor:
-    Gabriel Takeda
-    Carlos Cezar
-    Jose Getulio
-"""
 
 class Labirinto:
-    def _init_(self):
-
-        """
-        Inicializa um objeto Labirinto com um tamanho predefinido.
-
-        O tamanho padrão do labirinto é 31x31.
-
-        Atributos:
-            self.x (int): Largura do labirinto.
-            self.y (int): Altura do labirinto.
-            self.labirinto (list): Representação do labirinto como uma matriz.
-        """
+    def __init__(self):
 
         # Tamanho do labirinto
         self.x = 15
         self.y = 15
-        
         # Cria o labirinto
-        self.labirinto = self.criar_labirinto()
-        self.jogador ='*'
-        self.labirinto[self.posicaoX][self.posicaoY] = self.jogador
-    
-    def verifica_vitoria(self):
-        return maze.x - 2 == maze.posicaoX and maze.y - 1 == maze.posicaoY
+        self.maze = self.criar_labirinto()
     
     def criar_labirinto(self):
-
-        """
-        Cria um labirinto aleatório usando o algoritmo de Prim's.
-
-        Retorna:
-            list: Uma matriz representando o labirinto com caminhos e paredes.
-        """
          
         # Inicializa uma matriz preenchida com paredes ('W')
         matriz = [['X' for _ in range(self.x)] for _ in range(self.y)]
@@ -90,69 +46,13 @@ class Labirinto:
         
         # Marca a célula de fim
         marca_visita(end_x, end_y)
-    
+
+        matriz[1][0] = '*'    
         return matriz
     
     def imprimir_labirinto(self):
         # Imprime o labirinto
         for i in range(self.x):
             for j in range(self.y):
-                print(self.labirinto[i][j], end=' ')
+                print(self.maze[i][j], end=' ')
             print()
-    
-    def mover_jogador(self, new_x, new_y):
-        try:
-            if self.labirinto[new_x][new_y] != 'X' and new_x > -1 and new_x < maze.x +1 and new_y > -1 and new_y < maze.y +1:
-                self.labirinto[new_x][new_y] = '*'
-                self.labirinto[self.posicaoX][self.posicaoY] = '0'
-                if self.y != new_y:
-                    self.posicaoY = new_y
-                if self.x != new_x:
-                    self.posicaoX = new_x
-            else:
-                print('MOVIMENTO INVALIDO')
-        except:
-            print('movimento invalido')
-
-'''def labirinto_in_game(maze):
-    while True:
-
-        key = msvcrt.getch()
-
-        
-        if key == b'\xe0':
-            key = msvcrt.getch()
-            if key == b'H':
-                maze.andar_para_cima()
-            elif key == b'P':
-                maze.andar_para_baixo()
-            elif key == b'K':
-                maze.andar_para_esquerda()
-            elif key == b'M':
-                maze.andar_para_direita()
-        elif key == b'w':
-            maze.andar_para_cima()
-        elif key == b'a':
-            maze.andar_para_esquerda()
-        elif key == b's':
-            maze.andar_para_baixo()
-        elif key == b'd':
-            maze.andar_para_direita()
-        elif key == b'q':
-            break
-
-        if maze.verifica_vitoria():
-            maze.imprimir_labirinto()
-            print('VITORIAA (/^▽^)/')
-            break
-        else:
-            print('+-'*15)
-            maze.imprimir_labirinto()
-'''
-        
-
-# Cria uma instância da classe Labirinto
-maze = Labirinto()
-# Imprime o labirinto
-maze.imprimir_labirinto()
-labirinto_in_game(maze)
